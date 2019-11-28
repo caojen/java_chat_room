730e652722a9e8ee5ad93b1025ff512a632b41ec
unknown <1364515349@qq.com>
2019-11-28 23:01:36 +0800

    add note

--- Backend/Backend/Backend.java
+++ Backend/Backend/Backend.java
@@ -3,5 +3,6 @@ package Backend.Backend;
 public class Backend {
   public static void start() {
     System.out.println("Please wait, the backend is beginning to get ready...");
+    // TODO: listen to http-request and call 'Urls'
   }
 }
