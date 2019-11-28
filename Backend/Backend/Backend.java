479a1eb1d704be9c881a9abbe630522d73c28ce7
unknown <1364515349@qq.com>
2019-11-28 21:22:11 +0800

    add make file

---
+++ Backend/Backend/Backend.java
@@ -0,0 +1,7 @@
+package Backend.Backend;
+
+public class Backend {
+  public static void start() {
+    System.out.println("Please wait, the backend is beginning to get ready...");
+  }
+}
---
+++ Backend/Main.java
@@ -0,0 +1,9 @@
+package Backend;
+
+import Backend.Backend.Backend;
+
+public class Main {
+  public static void main(String args[]) {
+    Backend.start();
+  }
+}
