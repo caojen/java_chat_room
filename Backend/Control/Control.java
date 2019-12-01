6141baf4df165af9eb132ae412c9da3a3ae87195
unknown <1364515349@qq.com>
2019-12-01 20:07:25 +0800

    control interface

--- Backend/Control/Control.java
+++ Backend/Control/Control.java
@@ -1,1 +1,40 @@
 package Backend.Control;
+
+public class Control {
+  
+  public static boolean create_participant(String name, String password) {
+
+  }
+
+  public static boolean create_room(String roomid, String owner) {
+
+  }
+  
+  public static boolean delete_room(String roomid) {
+
+  }
+
+  public static boolean add_participant(String roomid, String participant) {
+
+  }
+
+  public static boolean delete_participant(String roomid, String participant) {
+
+  }
+
+  public static boolean add_message(String roomid, String newMessage) {
+
+  }
+
+  public static boolean get_message(String roomid, String time) {
+
+  }
+
+  public static boolean login(String username, String password) {
+
+  }
+
+  public static boolean login_required(String username, String token) {
+
+  }
+}
