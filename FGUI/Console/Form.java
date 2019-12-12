f8606aba75a5e0e8b2ee2d2b6142c3a351d96652
caojen <1364515349@qq.com>
2019-12-12 11:29:59 +0800

    update

--- FGUI/Console/Form.java
+++ FGUI/Console/Form.java
@@ -3,6 +3,8 @@ package FGUI.Console;
 import javax.swing.*;
 
 public class Form extends JFrame {
+  private static final long serialVersionUID = 8280050013528051744L;
+
   public static void start() {
 
   }
