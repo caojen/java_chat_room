40abcb6345879ba34c48f86094ac4b4025e23c44
caojen <1364515349@qq.com>
2019-12-12 11:45:50 +0800

    update

--- FGUI/Console/Form.java
+++ FGUI/Console/Form.java
@@ -5,7 +5,16 @@ import javax.swing.*;
 public class Form extends JFrame {
   private static final long serialVersionUID = 8280050013528051744L;
 
-  public static void start() {
+  public static Form form = new Form();
 
+  public static void start() {
+    Form.form.setVisible(true);
+  }
+
+  public Form() {
+    this.setTitle("Form");
+    this.setSize(400,500);
+    this.setLocationRelativeTo(null);
+    this.add(new JButton("This a Button"));
   }
 }
