86ba8b8703618bb7f6a866e72783ef79e3e33c76
caojen <1364515349@qq.com>
2019-12-13 17:57:33 +0800

    add fgui.ipswitch

---
+++ FGUI/Console/Components/IpSwitch.java
@@ -0,0 +1,45 @@
+package FGUI.Console.Components;
+
+import javax.swing.*;
+import java.awt.*;
+import FGUI.Console.Form;
+import FGUI.Configuation.Configuation;
+
+public class IpSwitch {
+  public static void start() {
+    Form.form.getContentPane().removeAll();
+    Form.form.setVisible(false);
+    Form.form.setTitle("Ip Switch");
+    
+    Container c = Form.form.getContentPane();
+
+    JPanel titlePanel = new JPanel();
+    titlePanel.setLayout(new FlowLayout());
+    titlePanel.add(new JLabel("Switch IP to Server"));
+    c.add(titlePanel, "North");
+
+
+    JLabel ip_label = new JLabel("Connect to:");
+    JTextField ip_text = new JTextField();
+    JPanel bodyPanel = new JPanel();
+    bodyPanel.setLayout(null);
+    ip_label.setBounds(50, 20, 100, 20);
+    bodyPanel.add(ip_label);
+    ip_text.setText(Configuation.ApiPrifix);
+    ip_text.setBounds(180, 20, 200, 20);
+    bodyPanel.add(ip_text);
+    c.add(bodyPanel);
+
+
+    JPanel buttomPanel = new JPanel();
+    buttomPanel.setLayout(new FlowLayout());
+    JButton jButton = new JButton();
+    jButton.setText("OK");
+    buttomPanel.add(jButton);
+    c.add(buttomPanel, "South");
+
+    Form.form.setSize(400, 160);
+
+    Form.form.setVisible(true);
+  }
+}
--- FGUI/Console/Form.java
+++ FGUI/Console/Form.java
@@ -3,36 +3,21 @@ package FGUI.Console;
 import javax.swing.*;
 import java.awt.Component;
 
+import FGUI.Console.Components.*;
+
 public class Form extends JFrame {
   private static final long serialVersionUID = 8280050013528051744L;
 
   public static Form form = new Form();
 
   public static void start() {
-    Form.form.setVisible(true);
+    IpSwitch.start();
   }
 
   public Form() {
     this.setTitle("Form");
     this.setSize(400,500);
     this.setLocationRelativeTo(null);
-  }
-
-  /**
-   * To clean the form
-   */
-  public void fresh() {
-    this.removeAll();
-  }
-
-  /**
-   * to fresh and add new component
-   * @param newCmp the component that will add to the Jframe
-   */
-  public void fresh(Component newCmp) {
-    this.removeAll();
-    this.setVisible(false);
-    this.add(newCmp);
-    this.setVisible(true);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
   }
 }
